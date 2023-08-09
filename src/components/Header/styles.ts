5b31ec2fde740717ee668a902812fb18b4e53c03
alana.araujo <alana.araujo@tce.ma.gov.br>
2023-08-09 10:48:36 -0300

    [HEADER FEITO]

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -17,3 +17,20 @@ export const HeaderContent = styled.div`
 
 
 `;
+
+
+export const NewTransactionButton = styled.button`
+    height: 50px;
+    boder: 0;
+    background: ${props => props.theme["green-500"]};
+    color: ${props => props.theme.white};
+    font-weight: bold;
+    padding: 0 1.25rem;
+    border-radius: 6px;
+    cursor: pointer;
+
+    &:hover {
+        background: ${props => props.theme["green-700"]};
+        transition: background-color: 0.2s;
+    }
+`;
