845e48883ae1a978c705eabf01d05fb65fcfdba2
alana.araujo <alana.araujo@tce.ma.gov.br>
2023-08-09 08:58:09 -0300

    [COMEÇANDO HEADER]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Transactions } from "../src/pages/Transactions"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
+      <Transactions/>
       <h1>Hello World</h1>
     </ThemeProvider>
   )
